d336ca8fbf8dd7e388b72d9bfcb01651e2b94b65
shubham <singhshubham3006.ss@gmail.com>
2023-07-28 00:35:15 +0530

    Added food cells and growing snake on eating food cells | used useRef direction because state does not render in event listner

--- src/components/Board.js
+++ src/components/Board.js
@@ -51,21 +51,53 @@ function Board() {
     };
     const isOutOfBounds = (coords, board) => {
         const { x, y } = coords;
-        if (x < 0 || y < 0) return true;
+        if (x <= 0 || y <= 0) return true;
         if (x > board.length || y > board[0].length) return true;
         return false;
     };
+    const generateFoodCells = (min, max) => {
+        const foodCell = {
+            x: Math.floor(Math.random() * (max - min + 1) + min),
+            y: Math.floor(Math.random() * (max - min + 1) + min)
+        }
+        const foodCellValue = getValueFromCoordinates(foodCell);
+        if (snakeSet.has(foodCellValue)) {
+            return generateFoodCells(min, max);
+        }
+        else return foodCell
+    }
+    const handleGameOver = (message) => {
+        setGameOverMessage(message);
+        setGameOver(true);
+        return;
+    }
     const moveSnake = () => {
         // next head coordinates 
         const tailOfSnakeCoords = snakeArr[snakeArr.length - 1];
         const nextHeadCoords = getCoordsInDirection(snakeArr[0], direction);
+        const nextHeadValue = getValueFromCoordinates(nextHeadCoords);
         if (isOutOfBounds(nextHeadCoords, board)) {
-            setGameOver(true);
+            handleGameOver("OOPS !!! You Went Outside the boundary");
+            return;
+        }
+        if (snakeSet.has(nextHeadValue)) {
+            handleGameOver("STUPID !!! You ate Yourself ");
+            return;
+        }
+        if (nextHeadCoords.x == foodCell.x && nextHeadCoords.y == foodCell.y) {
+            // Grow Snake
+            setScore((score) => score + 10);
+            const newSnakeSet = new Set([...snakeSet]);
+            newSnakeSet.add(nextHeadValue);
+            setSnakeSet(newSnakeSet);
+            const newSnakeArr = [nextHeadCoords, ...snakeArr];
+            setSnakeArr(newSnakeArr);
+            setFoodCell(generateFoodCells(1, boardSize))
             return;
         }
         // move snake array to next head coordinates
         const tailValue = getValueFromCoordinates(tailOfSnakeCoords);
-        const nextHeadValue = getValueFromCoordinates(nextHeadCoords);
+
         const newSnakeSet = new Set([...snakeSet]);
         newSnakeSet.delete(tailValue);
         newSnakeSet.add(nextHeadValue);
@@ -74,36 +106,52 @@ function Board() {
         const newSnakeArr = [nextHeadCoords, ...snakeArr];
         newSnakeArr.pop();
         setSnakeArr(newSnakeArr);
-        console.log(snakeArr);
     }
+    const handleDirectionChange = (e) => {
+        // console.log("direction Ref -> ", directionRef);
+        if (e.key === 'ArrowUp' && directionRef.current != "DOWN") {
+            setDirection("UP");
+            directionRef.current = "UP";
+        }
+        if (e.key === 'ArrowRight' && directionRef.current != "LEFT") {
+            setDirection("RIGHT");
+            directionRef.current = "RIGHT";
+        }
+        if (e.key === 'ArrowDown' && directionRef.current != "UP") {
+            setDirection("DOWN");
+            directionRef.current = "DOWN";
+        }
+        if (e.key === 'ArrowLeft' && directionRef.current != "RIGHT") {
+            setDirection("LEFT");
+            directionRef.current = "LEFT";
+        }
+    }
+
     const [board, setBoard] = useState(createBoard(boardSize))
     const [score, setScore] = useState(0);
     const [snakeArr, setSnakeArr] = useState([{ x: 5, y: 3 }, { x: 5, y: 2 }, { x: 5, y: 1 }]);
     const [snakeSet, setSnakeSet] = useState(new Set([51, 50, 49]));
-    const [direction, setDirection] = useState("RIGHT")
+    const [direction, setDirection] = useState("RIGHT");
+    const directionRef = useRef(direction);
+    const [foodCell, setFoodCell] = useState(generateFoodCells(1, boardSize));
     const [pause, setPause] = useState(false);
     const [gameOver, setGameOver] = useState(false);
+    const [gameOverMessage, setGameOverMessage] = useState("");
     useEffect(() => {
-        // console.log(direction, "First Render");
-        window.addEventListener("keydown", (e) => {
-            if (e.key === 'ArrowUp') setDirection("UP")
-            if (e.key === 'ArrowRight') setDirection("RIGHT")
-            if (e.key === 'ArrowDown') setDirection("DOWN")
-            if (e.key === 'ArrowLeft') setDirection("LEFT")
-            console.log(direction);
-        })
-
+        window.addEventListener("keydown", handleDirectionChange)
     }, [])
 
     useInterval(() => {
         if (!pause) {
             moveSnake();
         }
-    }, 500);
+    }, 300);
     if (gameOver) {
         return (
             <div>
                 <p>Game-Over Bitch!</p>
+                <h1>{score} </h1>
+                <h3>{gameOverMessage}</h3>
                 <button className='button' onClick={() => {
                     setGameOver(false);
                     setBoard(createBoard(boardSize));
@@ -135,9 +183,13 @@ function Board() {
                         return (
                             <div key={rowInd} className='row'>
                                 {row.map((element, index) => {
-                                    return (<div key={index} className={snakeSet.has(element) ? 'cell cell-red' : 'cell'}>
+                                    return (
+                                        <div key={index}
+                                            className={`cell ${snakeSet.has(element) ? "cellRed" : ""} ${element == getValueFromCoordinates(foodCell) ? "cellGreen" : ""} `}
+                                        >
 
-                                    </div>)
+                                        </div>
+                                    )
                                 })}
                             </div>
                         )
